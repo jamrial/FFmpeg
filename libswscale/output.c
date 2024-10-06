0c633b6728e2b57f175aa61dd425c20ccd7d2bb0
James Almer <jamrial@gmail.com>
2024-10-06 14:47:52 -0300

    swscale/output: add AYUV output support
    Signed-off-by: James Almer <jamrial@gmail.com>

--- libswscale/output.c
+++ libswscale/output.c
@@ -2825,6 +2825,59 @@ yuv2vuyX_X_c(SwsContext *c, const int16_t *lumFilter,
     }
 }
 
+static void
+yuv2ayuv_X_c(SwsContext *c, const int16_t *lumFilter,
+             const int16_t **lumSrc, int lumFilterSize,
+             const int16_t *chrFilter, const int16_t **chrUSrc,
+             const int16_t **chrVSrc, int chrFilterSize,
+             const int16_t **alpSrc, uint8_t *dest, int dstW, int y)
+{
+    int i;
+
+    for (i = 0; i < dstW; i++) {
+        int j;
+        int Y = 1 << 18, U = 1 << 18;
+        int V = 1 << 18, A = 255;
+
+        for (j = 0; j < lumFilterSize; j++)
+            Y += lumSrc[j][i] * lumFilter[j];
+
+        for (j = 0; j < chrFilterSize; j++)
+            U += chrUSrc[j][i] * chrFilter[j];
+
+        for (j = 0; j < chrFilterSize; j++)
+            V += chrVSrc[j][i] * chrFilter[j];
+
+        Y >>= 19;
+        U >>= 19;
+        V >>= 19;
+
+        if (Y  & 0x100)
+            Y = av_clip_uint8(Y);
+        if (U  & 0x100)
+            U = av_clip_uint8(U);
+        if (V  & 0x100)
+            V = av_clip_uint8(V);
+
+        if (alpSrc) {
+            A = 1 << 18;
+
+            for (j = 0; j < lumFilterSize; j++)
+                A += alpSrc[j][i] * lumFilter[j];
+
+            A >>= 19;
+
+            if (A & 0x100)
+                A = av_clip_uint8(A);
+        }
+
+        dest[4 * i    ] = A;
+        dest[4 * i + 1] = Y;
+        dest[4 * i + 2] = U;
+        dest[4 * i + 3] = V;
+    }
+}
+
 #define output_pixel(pos, val, bits) \
     AV_WL16(pos, av_clip_uintp2(val >> shift, bits) << output_shift);
 
@@ -3376,6 +3429,9 @@ av_cold void ff_sws_init_output_funcs(SwsContext *c,
         break;
     case AV_PIX_FMT_AYUV64LE:
         *yuv2packedX = yuv2ayuv64le_X_c;
+        break;
+    case AV_PIX_FMT_AYUV:
+        *yuv2packedX = yuv2ayuv_X_c;
         break;
     case AV_PIX_FMT_VUYA:
     case AV_PIX_FMT_VUYX:
