c7424e14bd735def18a004a3978f02f9a4194aa7
James Almer <jamrial@gmail.com>
2025-02-19 12:12:20 -0300

    avcodec: remove deprecated FF_API_TICKS_PER_FRAME
    Deprecated since 2023-05-15.
    
    Signed-off-by: James Almer <jamrial@gmail.com>

--- libavcodec/msmpeg4enc.c
+++ libavcodec/msmpeg4enc.c
@@ -284,13 +284,7 @@ void ff_msmpeg4_encode_ext_header(MpegEncContext * s)
     if (s->avctx->framerate.num > 0 && s->avctx->framerate.den > 0)
         fps = s->avctx->framerate.num / s->avctx->framerate.den;
     else {
-FF_DISABLE_DEPRECATION_WARNINGS
-        fps = s->avctx->time_base.den / s->avctx->time_base.num
-#if FF_API_TICKS_PER_FRAME
-            / FFMAX(s->avctx->ticks_per_frame, 1)
-#endif
-            ;
-FF_ENABLE_DEPRECATION_WARNINGS
+        fps = s->avctx->time_base.den / s->avctx->time_base.num;
     }
 
     put_bits(&s->pb, 5, FFMIN(fps, 31)); //yes 29.97 -> 29
